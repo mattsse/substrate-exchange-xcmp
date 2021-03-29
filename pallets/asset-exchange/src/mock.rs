fc4f46319858c9194652086426fefa95117ee97d
Matthias Seitz <matthias.seitz@outlook.de>
2021-03-29 14:17:40 +0200

    add exchange pallet tests

--- pallets/asset-exchange/src/mock.rs
+++ pallets/asset-exchange/src/mock.rs
@@ -1,4 +1,3 @@
-use crate as pallet_template;
 use frame_support::parameter_types;
 use frame_system as system;
 use sp_core::H256;
@@ -6,6 +5,8 @@ use sp_runtime::{
     testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
 };
+
+use crate as pallet_asset_exchange;
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
@@ -18,7 +19,8 @@ frame_support::construct_runtime!(
         UncheckedExtrinsic = UncheckedExtrinsic,
     {
         System: frame_system::{Module, Call, Config, Storage, Event<T>},
-        TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+        Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>},
+        AssetExchangeModule: pallet_asset_exchange::{Module, Call, Storage, Event<T>}
     }
 );
 
@@ -31,7 +33,6 @@ impl system::Config for Test {
     type BaseCallFilter = ();
     type BlockWeights = ();
     type BlockLength = ();
-    type DbWeight = ();
     type Origin = Origin;
     type Call = Call;
     type Index = u64;
@@ -43,23 +44,79 @@ impl system::Config for Test {
     type Header = Header;
     type Event = Event;
     type BlockHashCount = BlockHashCount;
+    type DbWeight = ();
     type Version = ();
     type PalletInfo = PalletInfo;
-    type AccountData = ();
+    type AccountData = pallet_balances::AccountData<u128>;
     type OnNewAccount = ();
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
     type SS58Prefix = SS58Prefix;
 }
 
-impl pallet_template::Config for Test {
+parameter_types! {
+    pub const InitialSharesSupply: u128 = 1_000_000_000_000_000_000_000_000;
+    pub const FeeDivisor:u128 = 10_000;
+    pub const MaxPoolLimit:u64 = 100_000_000;
+    pub const ExchangeFee:u128 = 25;
+    pub const ReferralFee:u128 = 0;
+    pub const MinNativeAssetAmount:u128 = 0;
+    pub const MinParachainAssetAmount:u128 = 0;
+}
+
+impl pallet_asset_exchange::Config for Test {
     type Event = Event;
+    type ExchangeAdmin = frame_system::EnsureRoot<Self::AccountId>;
+    type PoolId = u64;
+    type MaxPoolLimit = MaxPoolLimit;
+    type AssetId = u64;
+    type Currency = pallet_balances::Module<Test>;
+    type FeeDivisor = FeeDivisor;
+    type ExchangeFee = ExchangeFee;
+    type ReferralFee = ReferralFee;
+    type MinNativeAssetAmount = MinNativeAssetAmount;
+    type MinParachainAssetAmount = MinParachainAssetAmount;
+    type InitSharesSupply = InitialSharesSupply;
+}
+
+parameter_types! {
+    pub const ExistentialDeposit: u128 = 500;
+}
+
+/// Balance of an account.
+impl pallet_balances::Config for Test {
+    type Balance = u128;
+    type DustRemoval = ();
+    type Event = Event;
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = frame_system::Module<Test>;
+    type MaxLocks = ();
+    type WeightInfo = ();
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    system::GenesisConfig::default()
+    let mut t = frame_system::GenesisConfig::default()
         .build_storage::<Test>()
-        .unwrap()
-        .into()
+        .unwrap();
+    pallet_balances::GenesisConfig::<Test> {
+        balances: vec![
+            (1, 100_000),
+            (2, 100_000),
+            (3, 100_000),
+            (4, 100_000),
+            (5, 100_000),
+        ],
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+
+    pallet_asset_exchange::GenesisConfig::<Test> {
+        allowed_assets: vec![1, 2, 3, 4, 5],
+        exchange_account: Some(1),
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+
+    t.into()
 }
--- pallets/asset-exchange/src/tests.rs
+++ pallets/asset-exchange/src/tests.rs
@@ -1,23 +1,153 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use crate::{mock::*, AssetBalance, AssetSwap, Error};
+use frame_support::{assert_err, assert_ok};
 
 #[test]
-fn it_works_for_default_value() {
+fn change_exchange_beneficiary() {
     new_test_ext().execute_with(|| {
-        // Dispatch a signed extrinsic.
-        assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-        // Read pallet storage and assert an expected result.
-        assert_eq!(TemplateModule::something(), Some(42));
+        // set the account for exchange fees
+        assert_ok!(AssetExchangeModule::set_exchange_account(Origin::root(), 2));
+        assert_eq!(AssetExchangeModule::exchange_account(), Some(2));
     });
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn allow_assets() {
     new_test_ext().execute_with(|| {
-        // Ensure the expected error is thrown when no value is present.
-        assert_noop!(
-            TemplateModule::cause_error(Origin::signed(1)),
-            Error::<Test>::NoneValue
+        assert_ok!(AssetExchangeModule::allow_asset(Origin::root(), 5));
+        assert_ok!(AssetExchangeModule::allow_asset(Origin::root(), 6));
+        assert_ok!(AssetExchangeModule::allow_asset(Origin::root(), 7));
+        assert_ok!(AssetExchangeModule::allow_asset(Origin::root(), 8));
+
+        assert!(AssetExchangeModule::allowed_assets(&5).is_some());
+        assert!(AssetExchangeModule::allowed_assets(&6).is_some());
+        assert!(AssetExchangeModule::allowed_assets(&7).is_some());
+        assert!(AssetExchangeModule::allowed_assets(&8).is_some());
+    });
+}
+
+#[test]
+fn register_user_and_deposit_funds() {
+    new_test_ext().execute_with(|| {
+        // change the owner of the exchange, who receives the exchange fees
+        assert_ok!(AssetExchangeModule::set_exchange_account(Origin::root(), 3));
+
+        assert_ok!(AssetExchangeModule::register_user(Origin::signed(1)));
+        let deposit = AssetExchangeModule::deposited_amounts(&1).unwrap();
+        assert_eq!(deposit.amount, 0);
+        assert!(deposit.assets.is_empty());
+
+        // register assets for the user
+        assert_ok!(AssetExchangeModule::register_assets(
+            Origin::signed(1),
+            vec![1, 2]
+        ));
+        let deposit = AssetExchangeModule::deposited_amounts(&1).unwrap();
+        assert_eq!(deposit.assets.get(&1).cloned(), Some(0));
+        assert_eq!(deposit.assets.len(), 2);
+
+        // deposit funds in the user's account
+        assert_ok!(AssetExchangeModule::deposit(Origin::signed(1), 1, 100_000));
+        assert_ok!(AssetExchangeModule::deposit(Origin::signed(1), 2, 100_000));
+
+        let deposit = AssetExchangeModule::deposited_amounts(&1).unwrap();
+        assert_eq!(deposit.assets.get(&1).cloned(), Some(100_000));
+        assert_eq!(deposit.assets.get(&2).cloned(), Some(100_000));
+
+        // create a new poll
+        assert_ok!(AssetExchangeModule::add_basic_pool(
+            Origin::signed(1),
+            vec![1, 2],
+            3
+        ));
+        assert_eq!(AssetExchangeModule::pool_count(), Some(1));
+        assert!(AssetExchangeModule::pools(&1).is_some());
+
+        // add all funds from the user's account into the pool
+        assert_ok!(AssetExchangeModule::add_liquidity(
+            Origin::signed(1),
+            1,
+            vec![AssetBalance::new(1, 100_000), AssetBalance::new(2, 100_000)]
+        ));
+
+        let deposit = AssetExchangeModule::deposited_amounts(&1).unwrap();
+        assert_eq!(deposit.assets.get(&1).cloned(), Some(0));
+        assert_eq!(deposit.assets.get(&2).cloned(), Some(0));
+
+        // can't withdraw anymore
+        assert_err!(
+            AssetExchangeModule::withdraw(Origin::signed(1), 1, 100),
+            Error::<Test>::NotEnoughBalance
+        );
+
+        assert_ok!(AssetExchangeModule::register_user(Origin::signed(2)));
+        // register assets for the user
+        assert_ok!(AssetExchangeModule::register_assets(
+            Origin::signed(2),
+            vec![1, 2]
+        ));
+        assert_ok!(AssetExchangeModule::deposit(Origin::signed(2), 1, 100_000));
+        assert_ok!(AssetExchangeModule::deposit(Origin::signed(2), 2, 100_000));
+
+        // swap assets from user 2's account
+        assert_ok!(AssetExchangeModule::swap(
+            Origin::signed(2),
+            vec![AssetSwap {
+                pool_id: 1,
+                asset_in: 1,
+                amount_in: Some(10_000),
+                asset_out: 2,
+                min_amount_out: 9000
+            }],
+            None
+        ));
+
+        // ensure swapped assets are back in users account
+        let deposit = AssetExchangeModule::deposited_amounts(&2).unwrap();
+        assert_eq!(deposit.assets.get(&1).cloned(), Some(90_000));
+
+        // remove all liquidity from the pool
+        let pool = AssetExchangeModule::pools(&1).unwrap();
+        let share_balance = pool.share_balances(&1).cloned().unwrap();
+        assert_ok!(AssetExchangeModule::remove_liquidity(
+            Origin::signed(1),
+            1,
+            share_balance,
+            Vec::new()
+        ));
+
+        let deposit_liquidity_provider = AssetExchangeModule::deposited_amounts(&1).unwrap();
+
+        // remove all liquidity from the exchange beneficiary's account
+        let share_balance = pool.share_balances(&3).cloned().unwrap();
+        assert_ok!(AssetExchangeModule::remove_liquidity(
+            Origin::signed(3),
+            1,
+            share_balance,
+            Vec::new()
+        ));
+
+        let exchange_deposit = AssetExchangeModule::deposited_amounts(&3).unwrap();
+        // balance of exchange and provider liquidity provider should match swapped volume
+        assert_eq!(
+            exchange_deposit.assets.get(&1).cloned().unwrap()
+                + deposit_liquidity_provider.assets.get(&1).cloned().unwrap(),
+            110_000
+        );
+
+        let swapper_deposit = AssetExchangeModule::deposited_amounts(&2).unwrap();
+
+        // asset balances should match total deposited volume
+        assert_eq!(
+            exchange_deposit.assets.get(&1).cloned().unwrap()
+                + deposit_liquidity_provider.assets.get(&1).cloned().unwrap()
+                + swapper_deposit.assets.get(&1).cloned().unwrap(),
+            200_000
+        );
+        assert_eq!(
+            exchange_deposit.assets.get(&2).cloned().unwrap()
+                + deposit_liquidity_provider.assets.get(&2).cloned().unwrap()
+                + swapper_deposit.assets.get(&2).cloned().unwrap(),
+            200_000
         );
     });
 }
